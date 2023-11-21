465c05e0222c91a90bda9ea33537f0cc3d73127c
Hyennni <hyeeun22380@gmail.com>
2023-11-21 17:10:31 +0900

    list append 문법

--- 10. 오토마우스를 활용한 웹페이지 자동화/main10-1.py
+++ 10. 오토마우스를 활용한 웹페이지 자동화/main10-1.py
@@ -1,6 +1,61 @@
 import pyautogui
 import time
+import pyperclip
 
-while True:
-    print(pyautogui.position())
-    time.sleep(0.1)
+class Weather:
+    def __init__(self):
+        self.region =''
+
+    def set_region(self,region):
+        self.region = region
+
+    def _(self):
+        while True:
+            print(pyautogui.position())
+            time.sleep(0.1)
+
+    def search_naver_weather(self):
+
+        pyautogui.moveTo(1252, 182, 0.2)
+        pyautogui.click()
+        time.sleep(0.5)
+
+        pyperclip.copy(f"{self.region} 날씨")
+        pyautogui.hotkey("ctrl", "v")
+        time.sleep(0.5)
+
+        pyautogui.write(["enter"])
+        time.sleep(1)
+
+    def capture_naver_weather(self):
+
+        start_x = 990
+        start_y = 268
+        end_x = 1647
+        end_y = 940
+
+        pyautogui.screenshot(f'./{self.region} 날씨.png', region=(start_x, start_y, end_x - start_x, end_y - start_y))
+
+if __name__ == '__main__':
+    w = Weather()
+
+    while 1:
+        menu = input('0-종료 1-지역 입력 2-네이버 날씨 검색 3-날씨 캡처')
+        if menu =='0':
+            print('프로그램 종료')
+            break
+
+        elif menu=='1':
+            region = input('지역 이름 : ')
+            w.set_region(region)
+
+        elif menu=='2':
+            w.search_naver_weather()
+
+        elif menu=='3':
+            w.capture_naver_weather()
+            print('날씨 캡처 완료!')
+
+        else:
+            print('잘못된 번호입니다.')
+            continue
--- 12. 엑셀의 정보를 불러와 수료증 자동생성/main12-1.py
+++ 12. 엑셀의 정보를 불러와 수료증 자동생성/main12-1.py
@@ -1,11 +1,54 @@
 import pandas as pd
 
-df = pd.DataFrame([["홍길동", "1990.01.02", "2021-0001"],
-                    ["김민준", "1990.05.06", "2021-0002"],
-                    ["김철수", "2000.08.08", "2021-0003"],
-                    ["김영희", "2000.09.09", "2021-0004"],
-                    ["이서준", "2010.10.10", "2021-0005"],
-                    ["장다인", "2017.12.12", "2021-0006"]])
+class Certificate:
 
-print(df)
-df.to_excel(r'12. 엑셀의 정보를 불러와 수료증 자동생성\수료증명단.xlsx', index=False, header=False)
+    def __init__(self):
+        self.student = []
+    #     낱개로 담는 것보다 list로 종합해서 받는 게 나음 -> list는 복수형
+
+    def set_student(self, name,birth,c_number):
+        self.student = [name,birth,c_number]
+
+    def get_student(self):
+        return self.student
+
+    def save_to_excel(self):
+        df = pd.DataFrame([self.student])
+
+        df2 = pd.DataFrame([["홍길동", "1990.01.02", "2021-0001"],
+                           ["김민준", "1990.05.06", "2021-0002"],
+                           ["김철수", "2000.08.08", "2021-0003"],
+                           ["김영희", "2000.09.09", "2021-0004"],
+                           ["이서준", "2010.10.10", "2021-0005"],
+                           ["장다인", "2017.12.12", "2021-0006"]])
+
+
+        print(df)
+        df.to_excel(r'./certificate/수료증명단.xlsx', index=False, header=False)
+
+if __name__ == '__main__':
+
+    c = Certificate()
+    n = int(input('몇 명을 입력하실겁니까? '))
+    ls = []
+
+    for i in range(n):
+
+        name = input('이름 : ')
+        birth = input('생년월일 : ')
+        c_number = input('수료증번호 : ')
+
+        c.set_student(name,birth,c_number)
+        stu = c.get_student()
+        ls.append(stu)
+
+    for i in ls:
+        print(i)
+
+
+        # for j in stu:
+        #     print(j)
+
+    # c.save_to_excel()
+
+
